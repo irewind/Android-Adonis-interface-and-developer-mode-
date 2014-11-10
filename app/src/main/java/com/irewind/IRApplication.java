78b15db861d5e0237155a30cb0209c617dc23882
Constantin Vlad Iacob <cviacob@gmail.com>
2014-11-10 12:21:55 +0200

    first commit

---
+++ app/src/main/java/com/irewind/IRApplication.java
@@ -0,0 +1,7 @@
+package com.irewind;
+
+/**
+ * Created by tzmst on 11/10/2014.
+ */
+public class IRApplication {
+}
---
+++ app/src/main/java/com/irewind/activities/IRSplashActivity.java
@@ -0,0 +1,16 @@
+package com.irewind.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.irewind.R;
+
+
+public class IRSplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_irsplash);
+    }
+}
