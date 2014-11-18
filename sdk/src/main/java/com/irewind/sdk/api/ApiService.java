83182cb9dae45769aacba18be4a8cc87baca9284
Leonard Chioveanu <leonard.chioveanu@gmail.com>
2014-11-18 09:20:17 +0200

    Fix user caching. GET has @Query instead of @Field.

--- sdk/src/main/java/com/irewind/sdk/api/ApiService.java
+++ sdk/src/main/java/com/irewind/sdk/api/ApiService.java
@@ -11,6 +11,7 @@ import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.PATCH;
 import retrofit.http.POST;
+import retrofit.http.Query;
 
 public interface ApiService {
 
@@ -59,30 +60,30 @@ public interface ApiService {
 
     @GET("/rest/user/")
     void userById(@Header("Authorization") String authorization,
-                  @Field("id") String id,
+                  @Query("id") String id,
                   Callback<UserResponse> cb);
 
     @GET("/rest/user/search/findByEmail")
     void userByEmail(@Header("Authorization") String authorization,
-                     @Field("email") String email,
+                     @Query("email") String email,
                      Callback<UserResponse> cb);
 
     @GET("/rest/user/search/getAllActiveUsersOrderedByLastLoginDate")
     void users(@Header("Authorization") String authorization,
-               @Field("page") Integer page,
-               @Field("size") Integer size,
+               @Query("page") Integer page,
+               @Query("size") Integer size,
                Callback<UserResponse> cb);
 
     @GET("/user/changePassword")
     void changePassword(@Header("Authorization") String authorization,
-                        @Field("oldPassword") String oldPassword,
-                        @Field("newPassword") String newPassword,
-                        @Field("newPassword2") String newPassword2,
+                        @Query("oldPassword") String oldPassword,
+                        @Query("newPassword") String newPassword,
+                        @Query("newPassword2") String newPassword2,
                         Callback<UserResponse> cb);
 
     @DELETE("/user/mobileUnregister")
     void deleteAccount(@Header("Authorization") String authorization,
-                       @Field("id") String id,
+                       @Query("id") String id,
                        Callback<UserResponse> cb);
 
     @PATCH("/user/save-info")
@@ -106,38 +107,38 @@ public interface ApiService {
 
     @GET("/rest/video/search/findVideosWithPagination")
     void videosForUser(@Header("Authorization") String authorization,
-                       @Field("user") long userID,
-                       @Field("pageNo") Integer page,
-                       @Field("pageSize") Integer size,
+                       @Query("user") long userID,
+                       @Query("pageNo") Integer page,
+                       @Query("pageSize") Integer size,
                        Callback<Video> cb);
 
     @GET("/rest/video/search/findVideosWithPagination")
     void relatedVideos(@Header("Authorization") String authorization,
-                       @Field("videoId") long videoID,
-                       @Field("pageNo") Integer page,
-                       @Field("pageSize") Integer size,
+                       @Query("videoId") long videoID,
+                       @Query("pageNo") Integer page,
+                       @Query("pageSize") Integer size,
                        Callback<Video> cb);
 
     @GET("/rest/v2/search-videos")
     void searchVideos(@Header("Authorization") String authorization,
-                      @Field("pageNo") Integer page,
-                      @Field("pageSize") Integer size,
+                      @Query("pageNo") Integer page,
+                      @Query("pageSize") Integer size,
                       Callback<Video> cb);
 
     // --- Tags --- //
 
     @GET("/rest/video-tag/search/findTagsByVideo")
     void tagsForVideo(@Header("Authorization") String authorization,
-                      @Field("video") long videoID,
-                      @Field("pageNo") Integer page,
-                      @Field("pageSize") Integer size,
+                      @Query("video") long videoID,
+                      @Query("pageNo") Integer page,
+                      @Query("pageSize") Integer size,
                       Callback<Tag> cb);
 
     // --- Permissions --- // TODO
 
     @GET("/rest/v2/get-video-access/?permission=VIEW")
     void videoPermissionView(@Header("Authorization") String authorization,
-                             @Field("videoId") long videoID,
+                             @Query("videoId") long videoID,
                              Callback cb);
 
 
@@ -148,17 +149,17 @@ public interface ApiService {
 
     @GET("/user/updateUserNotification")
     void updateUserNotification(@Header("Authorization") String authorization,
-                                @Field("notificationType") String notificationType,
-                                @Field("status") boolean status,
+                                @Query("notificationType") String notificationType,
+                                @Query("status") boolean status,
                                 Callback cb);
 
     // --- Comments --- //
 
     @GET("/rest/v2/video-comment/list")
     void videoComments(@Header("Authorization") String authorization,
-                       @Field("videoId") long videoID,
-                       @Field("pageNo") Integer page,
-                       @Field("pageSize") Integer size);
+                       @Query("videoId") long videoID,
+                       @Query("pageNo") Integer page,
+                       @Query("pageSize") Integer size);
 
     @PATCH("/rest/video-comment")
     void postVideoComment(@Header("Authorization") String authorization,
