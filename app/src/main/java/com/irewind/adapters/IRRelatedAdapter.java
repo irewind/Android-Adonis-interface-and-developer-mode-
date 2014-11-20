11e8f4e804bb7e961e775bb1baf6e78f4bb57e81
Leonard Chioveanu <leonard.chioveanu@gmail.com>
2014-11-20 13:10:57 +0200

    Display related videos.

--- app/src/main/java/com/irewind/adapters/IRRelatedAdapter.java
+++ app/src/main/java/com/irewind/adapters/IRRelatedAdapter.java
@@ -10,62 +10,95 @@ import android.widget.TextView;
 
 import com.irewind.R;
 import com.irewind.models.RelatedItem;
+import com.irewind.sdk.model.Video;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by tzmst on 5/7/14.
  */
-public class IRRelatedAdapter extends ArrayAdapter<RelatedItem> {
+public class IRRelatedAdapter extends ArrayAdapter<Video> {
 
-    private List<RelatedItem> dataList;
+    private List<Video> videos;
     private int layout;
     private Context mContext;
+    private ImageLoader imageLoader;
 
-    public IRRelatedAdapter(Context context, int resource, List<RelatedItem> data) {
+    public IRRelatedAdapter(Context context, int resource, ImageLoader imageLoader) {
         super(context, resource);
         mContext = context;
         layout = resource;
-        dataList = data;
+        this.imageLoader = imageLoader;
     }
 
     @Override
-    public RelatedItem getItem(int position) {
-        return dataList.get(position);
+    public Video getItem(int position) {
+        return videos.get(position);
     }
 
     @Override
     public int getCount() {
-        return dataList.size();
+        return videos != null ? videos.size() : 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        AccountItemHolder holder;
+        VideoHolder holder;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(layout, null);
-            holder = new AccountItemHolder();
+            holder = new VideoHolder();
 
             holder.username = (TextView) convertView.findViewById(R.id.username);
             holder.likes = (TextView) convertView.findViewById(R.id.likes);
             holder.title = (TextView) convertView.findViewById(R.id.titleList);
             holder.date = (TextView) convertView.findViewById(R.id.date);
             holder.views = (TextView) convertView.findViewById(R.id.views);
-            holder.picture = (ImageView) convertView.findViewById(R.id.profileImage);
+            holder.picture = (ImageView) convertView.findViewById(R.id.movieImage);
             convertView.setTag(holder);
         } else {
-            holder = (AccountItemHolder) convertView.getTag();
+            holder = (VideoHolder) convertView.getTag();
         }
 
-        RelatedItem info = getItem(position);
+        Video video = videos.get(position);
+
+        if (video.getThumbnail() != null && video.getThumbnail().length() > 0) {
+            imageLoader.displayImage(video.getThumbnail(), holder.picture);
+        }
+        else {
+            holder.picture.setImageResource(R.drawable.ic_launcher);
+        }
+
+        holder.title.setText(video.getTitle() != null ? video.getTitle() : "");
+
+        holder.username.setText(video.getAuthorName() != null ? video.getAuthorName() : "");
+
+        holder.views.setText("" + video.getViews());
+        holder.likes.setText("" + video.getLikes());
 
         return convertView;
     }
 
-    private class AccountItemHolder {
+    private class VideoHolder {
         TextView title, username, date, likes, views;
         ImageView picture;
+    }
+
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+        notifyDataSetChanged();
+    }
+
+    public void appendVideos(List<Video> videos) {
+        if (this.videos == null) {
+            this.videos = new ArrayList<Video>(videos);
+        }
+        else {
+            this.videos.addAll(videos);
+        }
+        notifyDataSetChanged();
     }
 }
