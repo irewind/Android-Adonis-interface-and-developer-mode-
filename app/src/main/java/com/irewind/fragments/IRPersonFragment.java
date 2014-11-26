fa5807e612f9fa902b47efa6ebd0f778fc59d7e5
Leonard Chioveanu <leonard.chioveanu@gmail.com>
2014-11-26 13:17:11 +0200

    Prefix "Joined: " to user info.

--- app/src/main/java/com/irewind/fragments/IRPersonFragment.java
+++ app/src/main/java/com/irewind/fragments/IRPersonFragment.java
@@ -176,7 +176,7 @@ public class IRPersonFragment extends Fragment implements AdapterView.OnItemClic
                 profileImageView.setImageResource(R.drawable.img_default_picture);
             }
             nameTextView.setText(user.getDisplayName());
-            date.setText(DateUtils.getRelativeTimeSpanString(user.getCreatedDate(), new Date().getTime(), DateUtils.SECOND_IN_MILLIS));
+            date.setText("Joined: " + DateUtils.getRelativeTimeSpanString(user.getCreatedDate(), new Date().getTime(), DateUtils.SECOND_IN_MILLIS));
         } else {
             profileImageView.setImageResource(R.drawable.img_default_picture);
             nameTextView.setText("");
