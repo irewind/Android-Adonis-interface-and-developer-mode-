4aae641b2a6dc4240a6c598e5c62b9c2edf882bc
Leonard Chioveanu <leonard.chioveanu@gmail.com>
2015-02-10 14:35:59 +0200

    Use same activity for fullscreen video.

--- app/src/main/java/com/irewind/activities/IRMovieActivity.java
+++ app/src/main/java/com/irewind/activities/IRMovieActivity.java
@@ -1,45 +1,32 @@
 package com.irewind.activities;
 
-import android.annotation.TargetApi;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.view.Surface;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.irewind.R;
-import com.irewind.fragments.IRPersonFragment;
 import com.irewind.fragments.IRVideoDetailsFragment;
-import com.irewind.sdk.model.User;
 import com.irewind.sdk.model.Video;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-public class IRMovieActivity extends IRBaseActivity{
+public class IRMovieActivity extends IRBaseActivity {
 
-    public static ImageButton abBack, abSearch;
-    public static TextView abTitle;
-    public static Button abAction;
+    public ImageButton abBack, abSearch;
+    public TextView abTitle;
+    public Button abAction;
     private IRVideoDetailsFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iraccount_general_layout);
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            setTranslucentStatus(true);
-//            int statusBarHeight = (int) Math.ceil(80 * getResources().getDisplayMetrics().density);
-//            findViewById(R.id.activityRoot).setPadding(0, statusBarHeight, 0, 0);
-//        }
-//
-//        SystemBarTintManager tintManager = new SystemBarTintManager(this);
-//        tintManager.setStatusBarTintEnabled(true);
-//        tintManager.setNavigationBarTintEnabled(true);
 
         getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.nav_bar));
         getSupportActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
@@ -50,6 +37,8 @@ public class IRMovieActivity extends IRBaseActivity{
         getSupportActionBar().setHomeButtonEnabled(false);
         getSupportActionBar().setLogo(0);
 
+
+
         View view = getLayoutInflater().inflate(R.layout.actionbar, null);
         abTitle = (TextView) view.findViewById(R.id.title);
         abSearch = (ImageButton) view.findViewById(R.id.btn_search);
@@ -57,9 +46,36 @@ public class IRMovieActivity extends IRBaseActivity{
         abAction = (Button) view.findViewById(R.id.action);
         getSupportActionBar().setCustomView(view);
 
+        abTitle.setText(getString(R.string.movies));
+        abSearch.setVisibility(View.GONE);
+        abAction.setVisibility(View.GONE);
+
+        abBack.setVisibility(View.VISIBLE);
+        abBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                if (mFragment != null) {
+                    mFragment.stopPlayback();
+                }
+            }
+        });
+
+        int orientation = getScreenOrientation();
+
+        if (orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            getSupportActionBar().show();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            getSupportActionBar().hide();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
         if (savedInstanceState == null) {
-            mFragment = IRVideoDetailsFragment.newInstance();
-            mFragment.video = (Video) getIntent().getSerializableExtra("video");
+            mFragment = new IRVideoDetailsFragment();
+            mFragment.setVideo((Video) getIntent().getSerializableExtra("video"));
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, mFragment)
                     .disallowAddToBackStack()
@@ -68,26 +84,51 @@ public class IRMovieActivity extends IRBaseActivity{
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getSupportActionBar().show();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getSupportActionBar().hide();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
     }
 
-    @TargetApi(19)
-    private void setTranslucentStatus(boolean on) {
-        Window win = getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mFragment != null) {
+            mFragment.stopPlayback();
         }
-        win.setAttributes(winParams);
     }
 
     @Override
     public void onBackPressed() {
         finish();
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+    }
+
+    public int getScreenOrientation() {
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_270) {
+                return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+            } else {
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+            }
+        }
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_90) {
+                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+            } else {
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+            }
+        }
+        return ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
     }
 }
